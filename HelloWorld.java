f5d5b715659e0f845fd600257751b9ec7e61423a
Philipp Burri <philippburri@Philipps-iMac.local>
2020-10-31 00:33:33 +0100

    more testing

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
   public static void main(String[] args) {
-    System.out.println("Hello World my ass!");
+    System.out.println("Hello World my ass!, for real now");
   }
 }
